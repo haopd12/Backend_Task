07017af70124dbaca0e9f5acb7159767a69ebd32
haopd12 <hao.pd200200@sis.hust.edu.vn>
2022-10-20 22:43:27 +0700

    Novel Crawler

--- Import_to_Db.py
+++ Import_to_Db.py
@@ -1,29 +1,23 @@
-import sqlite3
-from JsonScrapper.Novel_Scrapper import read_json
-
-def Insert_to_Novel_Chapter(json_file="",connection=""):
-    cursor=connection.cursor()
-    json_obj=read_json(json_file)
-    columns=['Novel','Page','Page_Link']
-    for row in json_obj:
-        keys=tuple(row[c] for c in columns)
-        cursor.execute("insert into Novel_Chapter values(?,?,?)",keys) 
+def Insert_Novel(novel_info = [],connection=""):
+    cursor = connection.cursor()
+    columns = ['NID', 'Theme']
+    for row in novel_info:
+        keys = tuple(row[c] for c in columns)
+        cursor.execute("insert into Novel values(?,?)", keys) 
     
-def Insert_to_Novel_Info(json_file="Output/crawl_data_novel_base.json",connection=""):
-    cursor=connection.cursor()
-    json_obj=read_json(json_file)
-    columns=['Novel_Link']
-    for row in json_obj:
-        key=tuple(row[c] for c in columns)
-        cursor.execute("insert into Novel_Info values(?)",key) 
+def Insert_Chapter(chapter_info = [],connection=""):
+    cursor = connection.cursor()
+    columns = ['CID', 'Content', 'Page']
+    for row in chapter_info:
+        keys = tuple(row[c] for c in columns)
+        cursor.execute("insert into Chapter values(?,?,?)", keys) 
 
-def Insert_to_Belonged_to(json_file="Output/crawl_data_belonged_to_base.json",connection=""):
-    cursor=connection.cursor()
-    json_obj=read_json(json_file)
-    columns=['Novel','Novel_Link']
-    for row in json_obj:
-        keys=tuple(row[c] for c in columns)
-        cursor.execute("insert into Belonged_to values(?,?)",keys) 
+def Insert_Novel_Chapter(foreign_base = [], connection = ""):
+    cursor = connection.cursor()
+    columns = ['NID','CID']
+    for row in foreign_base:
+        keys = tuple(row[c] for c in columns)
+        cursor.execute("insert into Novel_Chapter values(?,?)", keys) 
         
 
     
